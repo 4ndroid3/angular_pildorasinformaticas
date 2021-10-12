e90762257ad441d5ca5607671617b8ea8fe212a5
4ndroid3 <andijaimo@gmail.com>
2021-10-12 15:13:18 -0300

    jugando con routes

--- app-empleados/src/app/app.component.ts
+++ app-empleados/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Empleado } from './empleado.model';
-import { EmpleadosService } from './empleados.service';
-import { ServicioEmpleadosService } from './servicio-empleados.service';
 
 @Component({
   selector: 'app-root',
@@ -10,35 +7,10 @@ import { ServicioEmpleadosService } from './servicio-empleados.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(
-    private miServicio: ServicioEmpleadosService,
-    private empleadosService: EmpleadosService
-  ) { }
   ngOnInit(): void {
-    this.empleados = this.empleadosService.empleados;
   }
-
-  empleados: Empleado[] = [];
 
   titulo = 'Listado de empleados';
-
-  cuadroNombre: string = '';
-  cuadroApellido: string = '';
-  cuadroCargo: string = '';
-  cuadroSalario: number = 0;
-
-  agregarEmpleado() {
-    let miEmpleado = new Empleado(
-      this.cuadroNombre, 
-      this.cuadroApellido, 
-      this.cuadroCargo, 
-      this.cuadroSalario
-    );
-    // this.miServicio.muestraMensaje(
-    //   `Se registró con exito el usuario: 
-    //   ${this.cuadroNombre} ${this.cuadroApellido}
-    //   con el cargo de ${this.cuadroCargo}`
-    // );
-    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
-  }
+  
+  
 }
